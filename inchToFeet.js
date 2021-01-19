b90a37664d4ca2e86e47e0bfaa273c9f829df23e
hasanmiaweb <hasanmiaweb@gmail.com>
2021-01-19 17:03:58 +0600

    first commit

---
+++ inchToFeet.js
@@ -0,0 +1,26 @@
+//Method 1 Start
+
+var user = [100, 120, 300];
+
+function inchToFeet(user) {
+  var feet = user / 12;
+  return feet;
+}
+
+var newUser = inchToFeet(user[0]);
+console.log(newUser);
+
+//Method 1 End
+// Method 2 Start
+function inchToFeet(inch) {
+  var feet = inch / 12;
+  return feet;
+}
+
+var user1 = inchToFeet(120);
+console.log(user1);
+
+var user2 = inchToFeet(100);
+console.log(user2);
+
+// Method 2 End
